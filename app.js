95df1b3d749cb231df5fb481a656a27bfa1894d5
kiran-code <ravikiransjce.code@gmail.com>
2021-01-10 14:07:32 +0530

    Improving UI

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ async function getRecentUrls(){
     // document.getElementById('getRecentBtn').classList.remove('hidden');
     document.getElementById('recentTableBody').innerHTML = `<tr>
                                                                 <th colspan="2">Please retry to fetch shortened urls</th>
-                                                                <th colspan="2"><button class="btn btn-primary float-right" id="getRecentBtn" onclick="getRecent()">Retry</button></th>
+                                                                <th style="text-align:center;" colspan="2"><button class="btn btn-primary float-right" id="getRecentBtn" onclick="getRecent()">Retry</button></th>
                                                             </tr>`;
     console.error(err);
   }
@@ -136,7 +136,7 @@ async function getRecentAllUrls(){
     // document.getElementById('getRecentAllBtn').classList.remove('hidden');
     document.getElementById('recentAllTableBody').innerHTML = `<tr>
                                                                     <th colspan="2">Please retry to fetch shortened urls</th>
-                                                                    <th style="text-align:right;" colspan="2"><button class="btn btn-primary float-right" id="getRecentAllBtn" onclick="getRecentAll()">Retry</button></th>
+                                                                    <th style="text-align:center;" colspan="2"><button class="btn btn-primary float-right" id="getRecentAllBtn" onclick="getRecentAll()">Retry</button></th>
                                                                 </tr>`;
     console.error(err);
   }
